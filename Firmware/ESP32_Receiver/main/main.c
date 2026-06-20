ba1ea1fa1d9dedb4c6ed0dea56519828a29d7dac
Mathieu Cartier <100831252+mathicarti@users.noreply.github.com>
2026-06-20 13:40:07 +1000

    PWM Servo Control
    Can control servo through ESP NOW
    will need to add queues mutex to stop bottle necks

--- Firmware/ESP32_Receiver/main/main.c
+++ Firmware/ESP32_Receiver/main/main.c
@@ -8,6 +8,9 @@
 #include "esp_now.h"
 #include "esp_mac.h"
 #include "esp_log.h"
+#include "driver/ledc.h"
+
+#define SERVO_GPIO GPIO_NUM_12
 
 static const char *TAG = "ESP_OUT";
 
@@ -19,7 +22,7 @@ typedef struct data_packet
 } data_packet;
 
 data_packet current_data = {0,0,0};
-data_packet rcv_packet;
+data_packet rcv_packet = {180, 0, 0};
 
 uint8_t mac_addr[ESP_NOW_ETH_ALEN] = {0xe4, 0x65, 0xb8, 0x75, 0xbb, 0x2c};
 
@@ -82,6 +85,63 @@ void init_esp_now(void)
     esp_now_add_peer(&peer);
 }
 
+uint32_t angle_to_duty(uint8_t angle)
+{
+    if (angle > 180) angle = 180;
+    // Maps angle (0deg -> 0.5ms, 180deg -> 2.5ms)
+    float pulse_width = 0.5 + (angle / 180.0) * 2.0;
+    // convert pulse width to duty cycle (12bit res. 50Hz)
+    uint32_t duty = (pulse_width / 20.0) * 4096;
+    return duty;
+}
+
+void servo_PWM_task(void *pvParameters)
+{
+    // init PWM for servo (id 0) and esc (id 1)
+    ledc_timer_config_t led_timer = {
+        .speed_mode = LEDC_LOW_SPEED_MODE, // Use Low-Speed mode (better stability)
+        .timer_num = LEDC_TIMER_0, // Timer 0 for PWM
+        .duty_resolution = LEDC_TIMER_12_BIT, // 12-bit resolution -> 4096 levels
+        .freq_hz = 50, // 50 Hz for SG90
+        .clk_cfg = LEDC_AUTO_CLK // Auto select clock source
+    };
+    ledc_timer_config(&led_timer);
+
+    ledc_channel_config_t ledc_channel = {
+        .channel = LEDC_CHANNEL_0,
+        .duty = 0,
+        .gpio_num = SERVO_GPIO,
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .hpoint = 0,
+        .timer_sel = LEDC_TIMER_0
+    };
+    ledc_channel_config(&ledc_channel);
+
+    while (1)
+    {
+        // need to change from rcv_packet to current but haven't implemented it yet
+        ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, angle_to_duty(rcv_packet.servo_angle));
+        ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+        vTaskDelay(10);
+    }
+}
+
+void esp_now_data_task(void *pvParameters)
+{
+    // init wifi
+    init_wifi();
+
+    // init esp_now
+    init_esp_now();
+
+    // Run the main processes to wait for callback
+    while (1)
+    {
+        esp_err_t result = esp_now_send(mac_addr, (uint8_t *)&current_data, sizeof(current_data));
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
 void app_main(void)
 {
     // init NVS
@@ -94,17 +154,7 @@ void app_main(void)
 
     gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
 
-    // init wifi
-    init_wifi();
-
-    // init esp_now
-    init_esp_now();
-
-    // Run the main processes to wait for callback
-    while (1)
-    {
-        esp_err_t result = esp_now_send(mac_addr, (uint8_t *)&current_data, sizeof(current_data));
-
-        vTaskDelay(pdMS_TO_TICKS(500));
-    }
+    // need to make some freeRTOS tasks as this is getting tricky
+    xTaskCreate(esp_now_data_task, "ESP NOW", 2048, NULL, 3, NULL);
+    xTaskCreate(servo_PWM_task, "Servo PWM", 2048, NULL, 5, NULL);
 }
